0f137060f589e8e02e7de78e49a87bdb2bb4e8cf
Julie K <julieskim160@gmail.com>
2018-03-01 12:10:11 -0800

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+/* global cuid */
+
+// eslint-disable-next-line no-unused-vars
+const store = {
+  bookmark: [
+    { id: cuid(), 
+      title: 'google maps', 
+      link:'https://www.google.com/maps', 
+      description:'mapquest but better', 
+      rating:'null'
+    },
+  ],
+  adding: false,
+  expand: null,
+  rating: null
+};
+  
+$(document).ready(function() {
+//   bookmark.render();
+});
